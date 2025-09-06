623cbfbdfd088fba5963a9a4f7a9309fe07d33dc
Harsh Koli <harshkoli1255@gmail.com>
2025-09-06 11:40:55 +0000

    still there is an error in this line check this out  data-new-gr-c-s-che

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
           'min-h-screen bg-background font-sans antialiased',
           ptSans.variable
         )}
+        suppressHydrationWarning={true}
       >
         {children}
         <Toaster />
